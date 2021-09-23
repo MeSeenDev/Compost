a14d7ed0ddb77ca544f6e46f3d7c438b67e71396
meseendev <revan@xakep.ru>
2021-09-24 01:14:56 +0300

    Initial commit

---
+++ app/src/main/java/ru/meseen/dev/compost/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package ru.meseen.dev.compost.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val descColor = Color(0xFFA5A5A5)
